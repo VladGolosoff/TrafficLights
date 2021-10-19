2bd43abaa28a8474b37719db8029258f42c81515
Vladislav Golosov <v.golosoff@gmail.com>
2021-10-19 23:40:05 +0300

    Alghoritm

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var button: UIButton!
+    var pressButton = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 75
+        redLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 75
+        yellowLight.alpha = 0.3
+        greenLight.layer.cornerRadius = 75
+        greenLight.alpha = 0.3
     }
 
+    @IBAction func buttonPressed() {
+        button.setTitle("Next", for: .normal)
+        
+        switch pressButton {
+        case 0 :
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            pressButton = 1
+        case 1:
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+            pressButton = 2
+        case 2:
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            pressButton = 0
+            
+        default:
+        break
+        }
+      
+        }
+    }
+    
 
-}
 

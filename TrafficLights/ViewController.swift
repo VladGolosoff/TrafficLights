8c191d44667696df59485d052b3321a8ae87d2ef
VladGolosoff <v.golosoff@gmail.com>
2021-11-19 17:30:25 +0300

    Final build!

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,22 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IB Outlets
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     @IBOutlet var button: UIButton!
     var pressButton = 0
     
+    //MARK: - Override Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = 75
         redLight.alpha = 0.3
-        yellowLight.layer.cornerRadius = 75
         yellowLight.alpha = 0.3
-        greenLight.layer.cornerRadius = 75
         greenLight.alpha = 0.3
     }
+    
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
 
+    //MARK: - IB Actions
+    
     @IBAction func buttonPressed() {
         button.setTitle("Next", for: .normal)
         
@@ -45,8 +54,9 @@ class ViewController: UIViewController {
         default:
         break
         }
-      
+        
         }
+    
     }
     
 
